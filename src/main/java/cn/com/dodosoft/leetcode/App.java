e1a91e7b14acdc79a1bfe013bf26e15a463f2fe1
dodo <dodo_oschina@163.com>
2019-03-06 16:15:58 +0800

    empty project

---
+++ src/main/java/cn/com/dodosoft/leetcode/App.java
@@ -0,0 +1,13 @@
+package cn.com.dodosoft.leetcode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
